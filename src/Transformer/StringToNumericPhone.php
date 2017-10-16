db987939476de537ef11cad0515d596102d4ccf8
George Ionov <g.ionov@php73.ru>
2017-10-16 16:35:10 +0400

    new phone transformer

---
+++ src/Transformer/StringToNumericPhone.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Project: transformers
+ * User: george
+ * Date: 16.10.17
+ */
+
+namespace NewInventor\Transformers\Transformer;
+
+
+use NewInventor\TypeChecker\TypeChecker;
+
+class StringToNumericPhone
+{
+    protected function transformInputValue($value)
+    {
+        $value = preg_replace('/\D/', '', $value);
+        if (strlen($value) !== 11) {
+            throw new \InvalidArgumentException('Phone should be string with 11 numbers');
+        }
+        
+        return $value;
+    }
+    
+    protected function validateInputTypes($value)
+    {
+        TypeChecker::check($value)->tstring()->fail();
+    }
+}
