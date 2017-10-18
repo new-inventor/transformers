e578af7929aec9bbd01171c316d77d6705899341
George Ionov <g.ionov@php73.ru>
2017-10-18 14:57:49 +0400

    new transformer

---
+++ src/Transformer/StringToSnakeCase.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Project: property-bag
+ * User: george
+ * Date: 28.09.17
+ */
+
+namespace NewInventor\Transformers\Transformer;
+
+
+use NewInventor\Transformers\Transformer;
+use NewInventor\TypeChecker\TypeChecker;
+
+class StringToSnakeCase extends Transformer
+{
+    protected function transformInputValue($value)
+    {
+        $value = preg_replace(['/[^_a-zA-Z0-9]+/', '/(?:_$|^_)/'], ['_', ''], $value);
+        $value = preg_replace_callback(
+            '/_?(?:[A-Z]+|\d+)/',
+            function ($matches) {
+                return '_' . $matches[0];
+            },
+            $value
+        );
+        $value = preg_replace(['/_+/', '/(?:^_+|_+$)/'], ['_', ''], $value);
+        
+        return strtolower($value);
+    }
+    
+    protected function validateInputTypes($value)
+    {
+        TypeChecker::check($value)->tstring()->fail();
+    }
+}
